48d265e3e9b969d349c5a3fc38dc00fb86ce8f6e
MarcusTJensen <31272825+MarcusTJensen@users.noreply.github.com>
2020-02-20 00:16:17 +0100

    .properties fix, datasource is now based on fields in the .properties field

--- Client/src/main/java/com/hk/exam/client/Client.java
+++ Client/src/main/java/com/hk/exam/client/Client.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         new Client().runClient();
     }
 
--- DB/src/main/java/com/hk/exam/database/PostgrresqlDataSource.java
+++ DB/src/main/java/com/hk/exam/database/PostgrresqlDataSource.java
@@ -1,30 +1,26 @@
 package com.hk.exam.database;
 
 import javax.sql.DataSource;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
-
-import org.postgresql.ds.PGPoolingDataSource;
 import org.postgresql.ds.PGSimpleDataSource;
 
 public class PostgrresqlDataSource {
     public DataSource getDataSource(){
         PGSimpleDataSource ds = new PGSimpleDataSource();
         Properties props = new Properties();
-        /*try (FileInputStream fis = new FileInputStream("postgresql.properties")){
-            props.load(fis);
+        String fName = "postgresql.properties";
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fName)){
+            props.load(inputStream);
             ds.setUrl(props.getProperty("url"));
-            ds.setUrl(props.getProperty("user"));
-            ds.setUrl(props.getProperty("pw"));
+            ds.setUser(props.getProperty("user"));
+            ds.setPassword(props.getProperty("password"));
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
-        ds.setUrl("jdbc:postgresql://localhost:5432/java_exam_db");
+        }
+        /*ds.setUrl("jdbc:postgresql://localhost:5432/java_exam_db");
         ds.setUser("postgres");
-        ds.setPassword("201197mtj");
+        ds.setPassword("201197mtj");*/
         return ds;
     }
 }
